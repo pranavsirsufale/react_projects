95a97d039b60fa68fdff5c74271d913599408de1
Pranav sirsufale <pranavdada334@gmail.com>
2024-02-25 20:25:17 +0530

    miner changes

--- basics/calculator/src/components/ButtonsContainer.jsx
+++ basics/calculator/src/components/ButtonsContainer.jsx
@@ -1,10 +1,11 @@
 import styles from './ButtonsContainer.module.css'
 const ButtonsContainer = () => {
-    return (<div className={styles.buttonsContainer}>
-        <button className={styles.button} > C </button>
-        <button className={styles.button} > 1 </button>
-        <button className={styles.button} > 2 </button>
-        <button className={styles.button} > * </button>
+
+    const buttonNames = ['c','1','2','*','3','4','-','5','6','=','7','8','/','9','0','+']
+
+
+    return (<div className={styles.ButtonsContainer}>
+        {buttonNames.map(buttonNames => <button className={styles.button}>{buttonNames}</button>)}
 
     </div>
 )};
